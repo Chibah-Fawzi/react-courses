2daf64790693823c14b7a4cf8f99f22d167b4e64
fawzi <fawzichibah@gmail.com>
2024-06-14 15:31:33 +0100

    initial push

---
+++ last-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      main: ["Work Sans"],
+      secondary: ["Roboto Condensed"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
