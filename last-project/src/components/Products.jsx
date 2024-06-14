e7b2bfb754c77078ecf7da670c3bbd44ed2348ff
fawzi <fawzichibah@gmail.com>
2024-06-14 16:33:02 +0100

    added products component

---
+++ last-project/src/components/Products.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Products({ products }) {
+  return (
+    <section className="text-center font-secondary my-[10vh]">
+      <h2 className="text-3xl font-bold uppercase">Products</h2>
+
+      <h4 className="text-xl font-bold uppercase my-20">
+        LOWER 48 STATES... $8.99 FLAT SHIPPING | FREE SHIPPING $99 OR MORE
+      </h4>
+
+      <h2 className="text-3xl font-bold uppercase">Product lists</h2>
+      <div className="grid grid-cols-3 gap-y-10 mt-10">
+        {products.map((produit) => (
+          <div className="flex flex-col items-center hover:underline">
+            <img
+              className="hover:opacity-70 cursor-pointer transition-all"
+              src={produit.image}
+              alt=""
+            />
+            <h2 className="text-xl">{produit.title}</h2>
+            <h4 className="text-lg font-bold">{produit.price}</h4>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
